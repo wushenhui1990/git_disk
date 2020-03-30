c92c6cb84d522b2cc4fe18b8df248d1bcd7e5877
sandy <sandyw@kaiyun-cn.com>
2020-03-30 15:43:57 +0800

    update

--- sh/replace_str_each_file.sh
+++ sh/replace_str_each_file.sh
@@ -5,18 +5,25 @@ echo "str1, str2, [path]"
 echo "str1 > str2, in path/or cur path if not set"
 exit 0
 fi
-echo "$1 > $2"
 
-if [ "$#" > 2 ];then
+if [ $# le 2 ];then
 FPATH=$3
 else
 FPATH=.
 fi
 
-find $FPATH | while read line;do
-    grep -s -q $1 $line 
-    if [ $? == 0 ]; then
-    echo replace $line
-    sed -i "s/$1/$2/g" $line
-    fi
-done
+if [ -f $FPATH]; then
+	grep -s -q "$1" $FPATH 
+	if [ $? == 0 ]; then
+	echo replace $FPATH
+	sed -i "s/$1/$2/g" $FPATH
+	fi
+else
+	find $FPATH |grep -v ".git" | while read line;do
+		grep -s -q "$1" $line 
+		if [ $? == 0 ]; then
+		echo replace $line
+		sed -i "s/$1/$2/g" $line
+		fi
+	done
+fi
